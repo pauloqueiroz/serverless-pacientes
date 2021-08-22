376f43bc262ca29dcd9f614f4c1dc8e151e758bf
Paulo Sérgio <pauloqueiroz16@gmail.com>
2021-08-22 19:54:25 -0300

    Criação de função para recuperar paciente por id

--- handler.js
+++ handler.js
@@ -23,3 +23,16 @@ module.exports.listarPacientes = async (event) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+
+module.exports.obterPacientes = async (event) => {
+  const { pacienteId } = event.pathParameters;
+  const paciente = pacientes.find((paciente) => paciente.id == pacienteId);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(paciente, null, 2),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
