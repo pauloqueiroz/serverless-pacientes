732e72f49423924b3484c298765577b68cba4a0c
Paulo Sérgio <pauloqueiroz16@gmail.com>
2021-08-24 00:29:31 -0300

    Implementação de função de cadastro de pacientes

--- handler.js
+++ handler.js
@@ -25,7 +25,7 @@ module.exports.listarPacientes = async (event) => {
 };
 
 
-module.exports.obterPacientes = async (event) => {
+module.exports.obterPaciente = async (event) => {
   const { pacienteId } = event.pathParameters;
   const paciente = pacientes.find((paciente) => paciente.id == pacienteId);
   if(!paciente){
@@ -41,4 +41,48 @@ module.exports.obterPacientes = async (event) => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+
+module.exports.cadastrarPaciente = async (event) => {
+  
+  const timestamp = new Date().getTime();
+
+  let dados = JSON.parse(event.body);
+
+  const { nome, data_nascimento, email, telefone } = dados;
+
+  const paciente = {
+    paciente_id: uuidv4(),
+    nome,
+    data_nascimento,
+    email,
+    telefone,
+    status: true,
+    criado_em: timestamp,
+    atualizado_em: timestamp,
+  };
+
+  try {
+
+    await dynamoDb
+      .put({
+        TableName: "PACIENTES",
+        Item: paciente,
+      })
+      .promise();
+
+    return {
+      statusCode: 201,
+    };
+  } catch (err) {
+    console.log("Error", err);
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error",
+      }),
+    };
+  }
 };
